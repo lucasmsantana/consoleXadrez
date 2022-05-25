02aadb3299f6e01a1e4b64e0876254f34bc006a0
Lucas Santana <lucasantana78@gmail.com>
2022-05-25 12:01:18 -0300

    Melhorando a implementação do metodo imprimirTabuleiro

--- ConsoleApp1/Tela.cs
+++ ConsoleApp1/Tela.cs
@@ -9,6 +9,7 @@ namespace ConsoleApp1
         {
             for (int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -17,7 +18,8 @@ namespace ConsoleApp1
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        Tela.imprimirPeca(tab.peca(i,j));
+                        Console.Write(" ");
 
                     }
 
@@ -25,6 +27,21 @@ namespace ConsoleApp1
 
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
