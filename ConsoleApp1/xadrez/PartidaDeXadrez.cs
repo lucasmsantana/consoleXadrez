9a7cea82b0af4eade379951956fe75ecd900fcbd
Lucas Santana <lucasantana78@gmail.com>
2022-06-01 11:26:19 -0300

    Alterando nome de método movimentoPossivel

--- ConsoleApp1/xadrez/PartidaDeXadrez.cs
+++ ConsoleApp1/xadrez/PartidaDeXadrez.cs
@@ -97,7 +97,7 @@ namespace ConsoleApp1.xadrez
 
         public void validarPosicaoDeDestino(Posicao origem, Posicao destino)
         {
-            if (!tab.peca(origem).podeMoverPara(destino))
+            if (!tab.peca(origem).movimentoPossivel(destino))
             {
                 throw new TabuleiroException("Posição de destino inválida!");
             }
