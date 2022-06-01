10f85a61626f14816ae40e48e22dc4de2f159115
Lucas Santana <lucasantana78@gmail.com>
2022-06-01 11:22:50 -0300

    Implementando a lógica de Xeque Mate

--- ConsoleApp1/Tela.cs
+++ ConsoleApp1/Tela.cs
@@ -16,13 +16,20 @@ namespace ConsoleApp1
             imprimirPecasCapturadas(partida);
             Console.WriteLine();
             Console.WriteLine("Turno: " + partida.turno);
-            Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
-            if (partida.xeque)
+            if (!partida.terminada)
             {
-                Console.WriteLine("XEQUE!");
+                Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                if (partida.xeque)
+                {
+                    Console.WriteLine("XEQUE!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("XEQUEMATE!");
+                Console.WriteLine("Vencedor: " + partida.jogadorAtual);
             }
         }
-       
         public static void imprimirPecasCapturadas(PartidaDeXadrez partida)
         {
             Console.WriteLine("Peças capturadas:");
